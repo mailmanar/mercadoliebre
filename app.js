550cd2c08fab89aaaa6242576969767a97ddc408
mailmanar <marianochena@gmail.com>
2022-04-18 00:45:53 -0300

    heroku

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express(); 
+
+
+const publicPath = path.resolve (__dirname, "./public") 
+app.use(express.static(publicPath))
+
+
+app.listen(process.env.PORT || 3000, () =>  
+console.log("Houston todo en orden en el puerto"));  
+
+app.get("/", (req, res) => { 
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/register", (req, res) => { 
+    res.sendFile(path.join(__dirname + "/views/register.html"))
+})
+
+app.get("/login", (req, res) => { 
+    res.sendFile(path.join(__dirname + "/views/login.html"))
+})
